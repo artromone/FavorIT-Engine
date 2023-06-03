43ae34ab951e1a1c1d7920cbf4eca6f5ad1c42fa
Artem <artrom170@gmail.com>
2023-06-03 20:14:14 +0300

    CG

--- main.cpp
+++ main.cpp
@@ -1,29 +1,28 @@
-#include <iostream>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 int main()
 {
-  size_t desktopWidth = sf::VideoMode::getDesktopMode().width;
-  size_t desktopHeight = sf::VideoMode::getDesktopMode().height;
-  //
-  size_t windowWidth = static_cast< size_t >(desktopWidth * 0.75);
-  size_t windowHeight = static_cast< size_t >(desktopHeight * 0.8);
-  //
-  sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "FavorIT_Engine", sf::Style::Titlebar);
-  //
-  int posX = static_cast< int >(desktopWidth / 2 - windowWidth / 2);
-  int posY = static_cast< int >(desktopHeight / 2 - windowHeight / 2);
+  sf::VideoMode desktopMode = sf::VideoMode::getDesktopMode();
+  size_t dWidth = desktopMode.width;
+  size_t dHeight = desktopMode.height;
+  if (dWidth == 0 || dHeight == 0)
+  {
+    dWidth = 800;
+    dHeight = 600;
+  }
+  size_t wWidth = static_cast< size_t >(dWidth * 0.75);
+  size_t wHeight = static_cast< size_t >(dHeight * 0.8);
+  sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "FavorIT_Engine", sf::Style::Titlebar);
+  int posX = dWidth / 2 - wWidth / 2;
+  int posY = dHeight / 2 - wHeight / 2;
   sf::Vector2i pos(posX, posY);
   sf::Vector2f posf(posX, posY);
   window.setPosition(pos);
   //window.setPosition(sf::Vector2i(sf::Window::CenterHorizontal(), sf::Window::CenterVertical()));
-  //
   sf::RectangleShape rectangle(sf::Vector2f(100, 100));
   rectangle.setFillColor(sf::Color::Red);
   rectangle.setPosition(posf);
-  //
   window.setVerticalSyncEnabled(true);
-  //
   while (window.isOpen())
   {
     sf::Event event;
