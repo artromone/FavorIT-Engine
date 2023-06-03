a3d448ece88d55b11dc2ac7ede14769cdaf956b1
Artem <artrom170@gmail.com>
2023-06-03 18:48:20 +0300

    added libs, game sample with blank window

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 int main()
 {
-  std::cout << "Hi\n";
+  sf::Window window(sf::VideoMode(800, 600), "FavorIT Engine");
+  window.setVerticalSyncEnabled(true);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+      {
+        window.close();
+      }
+    }
+  }
+  return 0;
 }
