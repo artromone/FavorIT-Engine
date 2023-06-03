a75b790ee456c61a8e67baba92e3116796440c5a
Artem <artrom170@gmail.com>
2023-06-03 19:31:32 +0300

    window centring

--- main.cpp
+++ main.cpp
@@ -2,8 +2,21 @@
 #include <SFML/Window.hpp>
 int main()
 {
-  sf::Window window(sf::VideoMode(800, 600), "FavorIT Engine");
+  size_t desktopWidth = sf::VideoMode::getDesktopMode().width;
+  size_t desktopHeight = sf::VideoMode::getDesktopMode().height;
+
+  size_t windowWidth = static_cast< size_t >(desktopWidth * 0.75);
+  size_t windowHeight = static_cast< size_t >(desktopHeight * 0.8);
+
+  sf::Window window(sf::VideoMode(windowWidth, windowHeight), "FavorIT Engine", sf::Style::Titlebar);
+
+  int posX = static_cast< int >(desktopWidth / 2 - windowWidth / 2);
+  int posY = static_cast< int >(desktopHeight / 2 - windowHeight / 2);
+  sf::Vector2i pos(posX, posY);
+  window.setPosition(pos);
+
   window.setVerticalSyncEnabled(true);
+
   while (window.isOpen())
   {
     sf::Event event;
